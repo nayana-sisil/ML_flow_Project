685f9dedc43836717ad5e544edafb23282a9928c
nayana sisil <nayanasisil@gmail.com>
2026-06-30 14:23:51 +0530

    model archtecture

--- model.py
+++ model.py
@@ -1,34 +1,45 @@
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
-class CIFAR10CNN(nn.Module):
-    def __init__(self, num_classes=10):
-        super().__init__()
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+
+        # Block 1
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
         self.bn1 = nn.BatchNorm2d(32)
+
+        # Block 2
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
         self.bn2 = nn.BatchNorm2d(64)
-        self.pool = nn.MaxPool2d(2, 2)
+
+        # Block 3
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
         self.bn3 = nn.BatchNorm2d(128)
-        self.pool2 = nn.AdaptiveAvgPool2d((4, 4))
+
+        self.pool = nn.MaxPool2d(2, 2)
+        self.dropout = nn.Dropout(0.3)
+
+        # Fully connected layers
         self.fc1 = nn.Linear(128 * 4 * 4, 256)
-        self.dropout = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(256, num_classes)
+        self.fc2 = nn.Linear(256, 10)
 
     def forward(self, x):
+        # 32x32 -> 16x16
         x = self.pool(F.relu(self.bn1(self.conv1(x))))
+
+        # 16x16 -> 8x8
         x = self.pool(F.relu(self.bn2(self.conv2(x))))
-        x = self.pool2(F.relu(self.bn3(self.conv3(x))))
-        x = torch.flatten(x, 1)
+
+        # 8x8 -> 4x4
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
+
+        x = x.view(x.size(0), -1)
+
         x = F.relu(self.fc1(x))
         x = self.dropout(x)
+
         x = self.fc2(x)
+
         return x
-
-
-def create_model(device="cpu"):
-    model = CIFAR10CNN()
-    return model.to(device)
